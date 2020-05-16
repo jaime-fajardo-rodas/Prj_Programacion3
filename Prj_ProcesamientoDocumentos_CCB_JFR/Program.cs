8e968d685dc615e791376b0aa16983950bcd0fe5
jaime-fajardo-rodas <jaimefajardorodas@gmail.com>
2020-05-16 17:22:49 -0500

    carga de proyecto inicial

---
+++ Prj_ProcesamientoDocumentos_CCB_JFR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prj_ProcesamientoDocumentos_CCB_JFR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
